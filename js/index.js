2689956b0fb2e77ee453b0aa60ccbcf4ee17dcf2
Diana <maritimdiane96@gmail.com>
2022-12-12 04:15:25 +0300

    completed challenge

--- js/index.js
+++ js/index.js
@@ -48,4 +48,80 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
     }
-  })
+  //fetch all the film details
+  const filmDetails = ()=>{
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+//iterate over all the movies in the list using for loop
+        console.log(data)
+        for(let i = 0; i < data.length; i++){
+            let item = data[i]
+            console.log(item)
+            const movieList = document.createElement("li")
+            const list = document.getElementById("showingMovie")
+
+            movieList.classList.add("list-group-item", "border", "border-info")
+
+            movieList.setAttribute('id',`${item.id}`)
+            movieList.innerText = item.title
+            console.log(item.title)
+
+
+            list.appendChild(movieList)
+
+            movieList.addEventListener('click',()=>{
+                const filmImage = document.getElementById("poster")
+                const filmTitle = document.getElementById("filmTitle")
+                const filmDescr = document.getElementById("movieDescription")
+                const runTime = document.getElementById("runtime")
+                const showTime = document.getElementById("showtime")
+                const availTickets =document.getElementById("ticketsAvailable")
+
+
+                filmImage.src = item.poster
+                filmTitle.innerText = item.title
+                filmDescr.textContent = item.decription
+                runTime.innerHTML =`Runtime:<span>${item.runtime}</span>`
+                showTime.innerText =`Showtime: ${item.showtime}`
+                availTickets.innerText =`Tickets available: (${item.capacity - item.tickets_sold})`
+
+                const ticketsBuy = document.getElementById("buyTicket")
+                let remTicket = Number(item.capacity - item.tickets_sold)
+
+                ticketsBuy.addEventListener('click',()=>{
+
+                    // const remaining tickets = ticket-1
+                    //decrementing the number of tickets after each purchase
+                    remTicket --
+                    if(remTicket <= 0){
+                        movieList.innerHTML =`${item.title} <span class="badge bg-danger">SOLD OUT</span>`
+
+                        availTickets.innerHTML = `Ticket available: <span class="badge bg-danger">SOLD OUT</span>`
+
+                    }else{
+
+                        availTickets.innerText = `Tickets available: (${ticket})`
+                    }
+
+                })
+
+
+
+            })
+
+
+        }
+
+
+
+
+    })
+
+}
+
+//call the functions
+filmDetails()
+moviePlaceHolder()
+
+})
